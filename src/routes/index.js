8898ba991f615a9829c9289f9872dbeb712b29a5
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2021-07-27 23:54:25 -0400

    Add bottomtab

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Home, SplashScreen, Detail } from '../screens'
+import { BottomBar } from '~/components'
+
+const routeIcons = {
+  Home: 'home-outline',
+  Search: 'search-outline',
+  Favorites: 'heart-outline',
+}
 
 export default function Routes() {
   const Stack = createStackNavigator()
